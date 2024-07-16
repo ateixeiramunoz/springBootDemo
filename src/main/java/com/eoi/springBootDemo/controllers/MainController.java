3b85507e3db055d972bebeee191767ba295164ab
Alejandro Teixeira Muñoz <mail.alejandro.teixeira@gmail.com>
2024-07-16 09:37:54 +0200

    Security 1 - 15/07/2024

---
+++ src/main/java/com/eoi/springBootDemo/controllers/MainController.java
@@ -0,0 +1,11 @@
+package com.eoi.springBootDemo.controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class MainController {
+
+    //@GetMapping("/usuarios")
+    //@PreAuthorize("hasAuthority('ADMIN')")
+
+}
